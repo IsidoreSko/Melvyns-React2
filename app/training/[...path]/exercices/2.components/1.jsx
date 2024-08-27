00f5ef92d0afce367751b64dfb3ddae2db54721e
IsidoreSKO <sparagmos@hotmail.fr>
2024-08-27 17:11:20 +0200

    first commit

--- app/training/[...path]/exercices/2.components/1.jsx
+++ app/training/[...path]/exercices/2.components/1.jsx
@@ -1,9 +1,10 @@
 export default function App() {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+    <ShoesList>
+      {/* // < className="grid grid-cols-1 gap-4 md:grid-cols-2"> */}
       {/* 🦁 Ceci est le premier composant */}
       {/* 🦁 Tu peux le copier/coller dans un nouveau composant pour le séparer */}
-      <div className="card w-full bg-base-300 shadow-xl">
+      {/* {      <div className="card w-full bg-base-300 shadow-xl">
         <figure>
           <img
             src="/images/shoes-1.png"
@@ -69,11 +70,71 @@ export default function App() {
           <h2 className="card-title">Darku Shoes</h2>
           <p>Wow, this shoes is so cool. You can wear it for any occasion.</p>
         </div>
+      </div>} */}
+      <ShoeCard
+        image="/images/shoes-1.png"
+        title="Shark Shoes"
+        description="This yellow shoes will make your friend jealous."
+        isNew={true}
+        // {On pourait juste mettre isNew}
+      />
+      <ShoeCard
+        image="/images/shoes-2.png"
+        title="Blue Wheti"
+        description="You can wear this shoes with any clothes. It will make you look cool."
+        isNew={true}
+      />
+      <ShoeCard
+        image="/images/shoes-3.png"
+        title="Basic Fit"
+        description="You know what? This shoes is the best shoes for you who like to walk."
+      />
+      <ShoeCard
+        image="/images/shoes-4.png"
+        title="Darku Shoes"
+        description="Wow, this shoes is so cool. You can wear it for any occasion."
+      />
+    </ShoesList>
+  );
+}
+
+// 🦁 Celui-ci va prendre des "props"
+// 🦁 Tu peux prendre une props `image`, `title` et `description`
+// 🦁 Créer un component `ShoeCard`
+function ShoeCard({ image, title, description, isNew }) {
+  return (
+    <div className="card w-full bg-base-300 shadow-xl">
+      <figure>
+        <img
+          src={image}
+          className="h-32 w-full object-cover object-center"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {title}
+          {/* <NewBadge isNew={isNew} />{} */}
+          {isNew ? <NewBadge /> : null}
+        </h2>
+        <p>{description}</p>
       </div>
     </div>
   );
 }
 
-// 🦁 Créer un component `ShoeCard`
-// 🦁 Celui-ci va prendre des "props"
-// 🦁 Tu peux prendre une props `image`, `title` et `description`
+function ShoesList({ children }) {
+  return (
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">{children}</div>
+  );
+}
+
+// function NewBadge({ isNew }) {
+//   if (!isNew) {
+//     return null;
+//   }
+//   return <div className="badge badge-secondary">NEW</div>;
+// }
+function NewBadge() {
+  return <div className="badge badge-secondary">NEW</div>;
+}
