c18656799ca6b6fff722d318d7f8aa9358662ee7
IsidoreSKO <sparagmos@hotmail.fr>
2024-09-12 14:10:09 +0200

    useEffect

--- app/training/[...path]/exercices/4.use-effect/8.jsx
+++ app/training/[...path]/exercices/4.use-effect/8.jsx
@@ -2,16 +2,46 @@
 "use client";
 
 import { User2 } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // 🦁 Créer une clé `STORAGE_KEY` qui est égale à `storage-name`
+const STORAGE_KEY = "storage-name";
+
+// const NameForm = ({ initialName }) => {
+//   // 🦁 Comme valeur initiale, récupère la valeur dans le localStorage
+//   const [name, setName] = useState(
+//     () => localStorage.getItem(STORAGE_KEY) ?? initialName
+//   );
+
+//   // 🦁 Créer un `useEffect` avec `name` comme dépendance
+//   // 🦁 Sauvegarde le `name` dans le localStorage avec la clé définie dans `STORAGE_KEY`
+//   useEffect(() => {
+//     localStorage.setItem(STORAGE_KEY, name);
+//   }, [name]);
+
+const getInitialStorageValue = (key, initialValue) => {
+  try {
+    // return localStorage.getItem(key) ?? initialValue;
+    return JSON.parse(localStorage.getItem(key)) ?? initialValue;
+  } catch {
+    return initialValue;
+  }
+};
+const useStickyState = (key, initialValue) => {
+  const [value, setValue] = useState(() =>
+    getInitialStorageValue(key, initialValue)
+  );
+
+  useEffect(() => {
+    // localStorage.setItem(key, value);
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value]);
+  return [value, setValue];
+};
 
 const NameForm = ({ initialName }) => {
   // 🦁 Comme valeur initiale, récupère la valeur dans le localStorage
-  const [name, setName] = useState(initialName);
-
-  // 🦁 Créer un `useEffect` avec `name` comme dépendance
-  // 🦁 Sauvegarde le `name` dans le localStorage avec la clé définie dans `STORAGE_KEY`
+  const [name, setName] = useStickyState(STORAGE_KEY, initialName);
 
   return (
     <div className="flex flex-col items-center justify-center">
--- app/training/[...path]/exercices/4.use-effect/9.jsx
+++ app/training/[...path]/exercices/4.use-effect/9.jsx
@@ -1,14 +1,72 @@
 "use client";
 
+import { useEffect } from "react";
 import { useState } from "react";
 
+// export default function App() {
+//   const [isCountingClick, setIsCountingClick] = useState(false);
+//   // eslint-disable-next-line no-unused-vars
+//   const [count, setCount] = useState(0);
+
+// 🦁 1 - Créer un `useEffect` qui vient écouter les click sur `window`
+
+// useEffect(() => {
+//   const onClick = () => {
+//     if (!isCountingClick) return;
+//     // setCount(count + 1);
+//     setCount((curr) => curr + 1);
+//   };
+//   document.addEventListener("click", onClick);
+//   return () => {
+//     document.removeEventListener("click", onClick);
+//   };
+// }, [isCountingClick]);
+
+// 2 - Pour optimiser:
+// useEffect(() => {
+//   // On place la condition en premier ce qui évite l'écoute de click quand isCountingClick est false
+//   if (!isCountingClick) return;
+//   const onClick = () => {
+//     setCount((curr) => curr + 1);
+//   };
+//   document.addEventListener("click", onClick);
+//   return () => {
+//     document.removeEventListener("click", onClick);
+//   };
+//   // 🦁 Ensuite il incrémente le state `count` uniquement si `isCountingClick` est `true`
+// }, [isCountingClick]);
+
+// 3 - Custome Hook:
+const useEventListener = ({
+  eventName,
+  handler,
+  element = window,
+  enabled = true,
+}) => {
+  useEffect(() => {
+    // On place la condition en premier ce qui évite l'écoute de click quand isCountingClick est false
+    if (!enabled) return;
+    const onClick = () => {
+      handler();
+    };
+    element.addEventListener(eventName, onClick);
+    return () => {
+      element.removeEventListener(eventName, onClick);
+    };
+    // 🦁 Ensuite il incrémente le state `count` uniquement si `isCountingClick` est `true`
+  }, [element, enabled, eventName, handler]);
+};
 export default function App() {
   const [isCountingClick, setIsCountingClick] = useState(false);
   // eslint-disable-next-line no-unused-vars
   const [count, setCount] = useState(0);
-
-  // 🦁 Créer un `useEffect` qui vient écouter les click sur `window`
-  // 🦁 Ensuite il incrémente le state `count` uniquement si `isCountingClick` est `true`
+  useEventListener({
+    eventName: "click",
+    handler: () => {
+      setCount((curr) => curr + 1);
+    },
+    enabled: isCountingClick,
+  });
 
   return (
     <div className="flex max-w-sm flex-col gap-8">
