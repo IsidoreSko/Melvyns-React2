5f9a482e028f33fc5e709b6c46015aad1940781a
IsidoreSKO <sparagmos@hotmail.fr>
2024-09-26 15:51:32 +0200

    memo

--- app/training/[...path]/exercices/6.hooks/15.jsx
+++ app/training/[...path]/exercices/6.hooks/15.jsx
@@ -2,22 +2,27 @@
 
 import { Search } from "lucide-react";
 /* eslint-disable no-unused-vars */ // 🦁 Enlève cette ligne
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const useDebounceFn = (callback, time) => {
   // 🦁 Remplace cette variable par un `useRef`
+  const debounce = useRef(null);
   // 💡 timeout correspond à la référence de notre timeout.
   //   Quand tu fais un setTimeout, il return une valeur que
   //   tu peux clear afin de l'annuler. https://developer.mozilla.org/fr/docs/Web/API/setTimeout#valeur_de_retour
-  const timeout = null;
+  let timeout = useRef(null);
 
   const onDebounce = (...args) => {
-    // 🦁 Annule le timeout https://developer.mozilla.org/en-US/docs/Web/API/clearTimeout
-    // ℹ️ Cette fonction sera appelée à chaque fois que l'user tape un caractère, on veut donc clear
-    //    le dernier timeout pour relancer un nouveau timeout.
-    // 🦁 Crée un nouveau timeout https://developer.mozilla.org/en-US/docs/Web/API/setTimeout
-    //    a la fin il doit appeler la callback avec les arguments et le temps est défini par le paramètre `time`
+    clearTimeout(timeout.current);
+    timeout.current = setTimeout(() => {
+      callback(...args);
+    }, time);
   };
+  // 🦁 Annule le timeout https://developer.mozilla.org/en-US/docs/Web/API/clearTimeout
+  // ℹ️ Cette fonction sera appelée à chaque fois que l'user tape un caractère, on veut donc clear
+  //    le dernier timeout pour relancer un nouveau timeout.
+  // 🦁 Crée un nouveau timeout https://developer.mozilla.org/en-US/docs/Web/API/setTimeout
+  //    a la fin il doit appeler la callback avec les arguments et le temps est défini par le paramètre `time`
 
   return onDebounce;
 };
@@ -26,25 +31,42 @@ const fetchAgeByName = (name) => {
   return fetch(`https://api.agify.io/?name=${name}`).then((res) => res.json());
 };
 
+let render = 0;
+const useRenderCount = () => {
+  const render = useRef(0);
+  return render.current++;
+};
+
 export default function App() {
   const [result, setResult] = useState(null);
+  const inputRef = useRef(null);
+
+  const onSearch = useDebounceFn((value) => {
+    fetchAgeByName(inputRef.current?.value).then((data) => {
+      setResult(data);
+      inputRef.current?.focus();
+    });
+  }, 500);
 
   // 🦁 Wrap la function `onSearch` dans le hooks useDebounce
   // 💡 const onSearch = useDebounce((value) => {...}, 500);
-  const onSearch = (value) => {
-    fetchAgeByName(value).then((data) => {
-      setResult(data);
-    });
-  };
+  // const onSearch = (value) => {
+
+  //   });
+  // };
+  const renderCount = useRenderCount();
 
   return (
     <div className="flex flex-col items-center gap-4">
+      <p>Render {renderCount} time</p>
       <label className="input input-bordered flex items-center gap-2">
         <input
+          ref={inputRef}
           type="text"
           placeholder="Search bar"
           onChange={(event) => {
-            onSearch(event.target.value);
+            // onSearch(event.target.value);
+            onSearch();
           }}
         />
         <Search size={16} />
--- app/training/[...path]/exercices/6.hooks/16.jsx
+++ app/training/[...path]/exercices/6.hooks/16.jsx
@@ -1,17 +1,41 @@
 "use client";
 import { SlowComponent } from "@/src/utils/SlowComponents";
 import { useState } from "react";
+// import { memo } from "react";
 
-export default function App() {
+// const SlowComponentMemo = memo(SlowComponent);
+
+const Counter = ({ children }) => {
   const [count, setCount] = useState(0);
-
   return (
     <div className="flex flex-col items-center gap-4 rounded-md border border-neutral bg-base-200 p-4 shadow-md">
       <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
         Click me
       </button>
-      <SlowComponent />
+      {children}
       <p>Count : {count}</p>
     </div>
   );
+};
+
+export default function App() {
+  // const [count, setCount] = useState(0);
+  return (
+    <div>
+      <Counter>
+        <SlowComponent />
+      </Counter>
+    </div>
+  );
+}
+{
+  /* // return (
+  //   <div className="flex flex-col items-center gap-4 rounded-md border border-neutral bg-base-200 p-4 shadow-md">
+  //     <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+  //       Click me
+  //     </button>
+  //     <SlowComponentMemo />
+  //     <p>Count : {count}</p>
+  //   </div>
+  // ); */
 }
